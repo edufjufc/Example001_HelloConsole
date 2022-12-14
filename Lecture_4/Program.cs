2772054a3ef2041b7de6e535c85ecd0562d05329
Nikol <nikol.klimova.2018@mail.ru>
2022-12-14 23:38:25 +0300

    lecture 4

---
+++ Lecture_4/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+
+string [,] table= new string[2, 5];
+// String.Empty
+// table[0, 0]  table[0, 1]  table[0, 2]
+// table[1, 0]  table[1, 1]  table[1, 2]
+
+table[1, 2]="слово";
+
+for(int rows=0; rows<2; rows++)
+{
+    for(int columns=0; columns<5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+
+*/
+
+/*
+
+void PrintArray(int[,] matr)
+{
+    for(int i=0; i< matr.GetLength(0); i++)
+    {
+       for(int j=0; j< matr.GetLength(1); j++)
+       {
+          Console.Write($"{matr[i, j]} ");
+       }
+
+       Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for(int i=0; i< matr.GetLength(0); i++)
+    {
+       for(int j=0; j< matr.GetLength(1); j++)
+       {
+           matr[i,j]= new Random().Next(1, 10);
+       }
+    }
+}
+
+int [,] matrix= new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+*/
+
+/*
+
+// Факториал
+
+double Factorial(int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+*/
+
+// Фибоначи 
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
